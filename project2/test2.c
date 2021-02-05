500d04dacd1a5cbe09fc5b94df32e27ca1bea0b8
BrianTsung <33406077+BrianTsung@users.noreply.github.com>
2021-02-05 17:17:01 +0800

    Update test2.c

--- project2/test2.c
+++ project2/test2.c
@@ -6,49 +6,39 @@
 #include <time.h>
 #include <string.h>
 
-void delay(int secs)
-{
-int retTime=time(0)+secs;
-while(time(0)<retTime);
-}
-int main(void)
-{
-
-delay(3);
-int i;
-int pid;
-pid=getpid();
-srand(time(NULL));
-int random_num;
-
-char pid_str[12];
-sprintf(pid_str, "%d", pid);
-char filename[17] = "fileyayaya_";
-strcat(filename, pid_str);
-strcat(filename, ".txt");
-FILE *fptr;
-fptr = fopen(filename,"a");
-for (i=1;i<=18;i++)
-{
-random_num=(rand()%3)+1;
-//printf("%d wake up\n", random_num);
-
-long retval2=syscall(353,random_num);
-
-if (retval2!=random_num)
-printf("0:wake up error\n");
-else
-printf("1: %dwake up successful\n", random_num);
-
-fprintf(fptr,"project2_queue%d wake up\n",random_num);
-delay(2);
-}
-syscall(353,1);
-syscall(353,2);
-syscall(353,3);
-fclose(fptr);
-//printf("%d wake up\n", random_num);
-
-return 1;
+void delay(int secs){
+  int retTime=time(0)+secs;
+  while(time(0)<retTime);
 }
 
+int main(void){
+  delay(3);
+  int i;
+  int pid;
+  pid=getpid();
+  srand(time(NULL));
+  int random_num;
+
+  char pid_str[12];
+  sprintf(pid_str, "%d", pid);
+  char filename[17] = "fileyayaya_";
+  strcat(filename, pid_str);
+  strcat(filename, ".txt");
+  FILE *fptr;
+  fptr = fopen(filename,"a");
+  for (i=1;i<=18;i++){
+    random_num=(rand()%3)+1;
+    long retval2=syscall(353,random_num);
+    if (retval2!=random_num)
+      printf("0:wake up error\n");
+    else
+      printf("1: %dwake up successful\n", random_num);
+    fprintf(fptr,"project2_queue%d wake up\n",random_num);
+    delay(2);
+  }
+  syscall(353,1);
+  syscall(353,2);
+  syscall(353,3);
+  fclose(fptr);
+  return 1;
+}
